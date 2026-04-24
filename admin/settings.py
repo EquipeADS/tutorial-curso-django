eb0712cd855dbd52b49cd3c010362674b58cfafa
EquipeADS <projetointegradoradsunisc@gmail.com>
2026-04-24 15:13:17 -0300

    arumadas!

--- admin/settings.py
+++ admin/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4_103)jpeh6vm6ay*kx3p!-8o8mo(jo(r!8zr+7040w@z&4#ul'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'transactions',
     'home',
+    
 ]
 
 MIDDLEWARE = [
